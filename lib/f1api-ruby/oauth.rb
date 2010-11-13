312f6ebeb4f7189718c5cbf0d7611253a4f01103
Jesse Dearing <jdearing@fellowshiptech.com>
2010-11-13 09:38:26 -0600

    Setting up OAuth object with configuration variables

--- lib/f1api-ruby/oauth.rb
+++ lib/f1api-ruby/oauth.rb
@@ -1,11 +1,16 @@
 require 'oauth'
 
-module FellowshipTechAPIClient # :nodoc:
+module FellowshipOneAPIClient # :nodoc:
   # Wrapper around the OAuth v1.0 specification using the +oauth+ gem.
   module OAuth
+    Configuration = ::FellowshipOneAPIClient::Configuration
     # This will get a new request token and return the authorize url
     def authorize!
-      @consumer = OAuth::Consumer.new
+      @consumer = ::OAuth::Consumer.new(Configuration.consumer_key, Configuration.consumer_secret,
+                  { :site => Configuration.site_url,
+                    :request_token_path => Configuration.request_token_path,
+                    :access_token_path => Configuration.access_token_path,
+                    :authorize_path => Configuration.portal_authorize_path})
     end
   end
 end
