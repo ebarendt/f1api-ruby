265316772f5292f1d01612cb2e0ee3033bb2d534
Jesse Dearing <jdearing@fellowshiptech.com>
2010-11-13 15:50:15 -0600

    Added a class eval for the OAuth library and added functionality to get the Content-Location header. Fixed some bugs

--- lib/f1api-ruby/oauth.rb
+++ lib/f1api-ruby/oauth.rb
@@ -3,7 +3,40 @@ require 'oauth'
 require 'base64'
 require 'uri'
 
-OAuth::Consumer.
+# The token request reponse is scoped only in the token_request method, but I need to get access to the response
+# headers so that I can pull back the Content-Location header and get the authenticated user URI
+OAuth::Consumer.class_eval do
+  def token_request(http_method, path, token = nil, request_options = {}, *arguments)
+    @tr_response = request(http_method, path, token, request_options, *arguments)
+    case @tr_response.code.to_i
+
+    when (200..299)
+      if block_given?
+        yield @tr_response.body
+      else
+        # symbolize keys
+        # TODO this could be considered unexpected behavior; symbols or not?
+        # TODO this also drops subsequent values from multi-valued keys
+        CGI.parse(@tr_response.body).inject({}) do |h,(k,v)|
+          h[k.strip.to_sym] = v.first
+          h[k.strip]        = v.first
+          h
+        end
+      end
+    when (300..399)
+      # this is a redirect
+      @tr_response.error!
+    when (400..499)
+      raise OAuth::Unauthorized, @tr_response
+    else
+      @tr_response.error!
+    end
+  end
+  
+  def token_request_response
+    @tr_response
+  end
+end
 
 module FellowshipOneAPIClient # :nodoc:
   # Wrapper around the OAuth v1.0 specification using the +oauth+ gem.
@@ -16,8 +49,12 @@ module FellowshipOneAPIClient # :nodoc:
     # Implements the pure OAuth method of authentication.  This allows the Fellowship One API to 
     # manage the authentication process.
     module OAuthAuthentication
+      # The URI for the resource of the authenticated user
+      attr_reader :authenticated_user_uri
+
       # The OAuth consumer object
-      attr_reader :oauth_consmer
+      attr_reader :oauth_consumer
+      alias :consumer :oauth_consumer
       
       # The OAuth request object
       attr_reader :oauth_request
@@ -42,8 +79,14 @@ module FellowshipOneAPIClient # :nodoc:
         @oauth_authorize_url = oauth_request.authorize_url
       end
       
+      # After a the user has been authenticated then we use the access token to access protected resources in the API.
+      # Since the authentication has taken place, we now know about the user that authenticated and 
+      # have a URI to the record of that user.
+      #
+      # The URI for the authenticated user is returned.
       def get_access_token
-        @oauth_access_token = oauth_requeset.get_access_token
+        @oauth_access_token = oauth_request.get_access_token
+        @authenticated_user_uri = oauth_consumer.token_request_response["Content-Location"]
       end
     end
     
