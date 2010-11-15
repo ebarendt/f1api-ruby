773e07f8fde704ed29ba006cfd88ef44a8771427
Jesse Dearing <jdearing@fellowshiptech.com>
2010-11-15 02:29:28 -0600

    Consolidated all the requires needed

--- lib/f1api-ruby.rb
+++ lib/f1api-ruby.rb
@@ -3,6 +3,9 @@ require 'yaml'
 require 'oauth'
 require 'base64'
 require 'uri'
-require "#{File.dirname(__FILE__)}/f1api-ruby/configuration.rb"
-require "#{File.dirname(__FILE__)}/f1api-ruby/oauth.rb"
-require "#{File.dirname(__FILE__)}/f1api-ruby/client.rb"
+require "#{File.dirname(__FILE__)}/f1api-ruby/configuration"
+require "#{File.dirname(__FILE__)}/f1api-ruby/oauth"
+require "#{File.dirname(__FILE__)}/f1api-ruby/oauth/credentials_authentication"
+require "#{File.dirname(__FILE__)}/f1api-ruby/oauth/oauth_authentication"
+require "#{File.dirname(__FILE__)}/f1api-ruby/client"
+require "#{File.dirname(__FILE__)}/f1api-ruby/base"
