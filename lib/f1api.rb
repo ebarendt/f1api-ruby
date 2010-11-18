c67af8c0743fd8653b41433d62cc2baae71fede2
Jesse Dearing <jdearing@fellowshiptech.com>
2010-11-18 17:57:33 -0600

    Added activeresource and associated f1api classes

--- lib/f1api.rb
+++ lib/f1api.rb
@@ -4,9 +4,11 @@ require 'yaml'
 require 'oauth'
 require 'base64'
 require 'uri'
+require 'active_resource'
 require "f1api/configuration"
 require "f1api/oauth"
 require "f1api/oauth/credentials_authentication"
 require "f1api/oauth/oauth_authentication"
 require "f1api/client"
-require "f1api/base"
+require "f1api/activeresource/connection"
+require "f1api/activeresource/base"
