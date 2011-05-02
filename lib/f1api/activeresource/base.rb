c6246cc01d1ab8af54dd38c6ed8f0940f12c22ac
Jesse Dearing <jdearing@fellowshiptech.com>
2011-05-01 22:12:14 -0500

    Starting to convert to JSON

--- lib/f1api/activeresource/base.rb
+++ lib/f1api/activeresource/base.rb
@@ -1,12 +1,11 @@
 module FellowshipOneAPI # :nodoc:
   # The Base class should be inherited by all model classes as it provides the facilities that the class will need
   class Base < ActiveResource::Base
-    self.site = "#{Configuration.site_url}/v1"
     # Creates a new connection
-    # 
+    #
     # ==Examples
     #   Person.connect(FellowshipOneAPI::Client.new)
-    # 
+    #
     # If the connection needs to be forcibly refreshed then you can pass true
     #   Person.connect(FellowshipOneAPI::Client.new, true)
     def self.connect(client, refresh = false)
@@ -15,10 +14,5 @@ module FellowshipOneAPI # :nodoc:
       end
     end
 
-    def initialize(*args)
-      template = @connection.request :get, self.site + "/" + self.class.to_s.pluralize + "/New.json"
-      template = JSON.parse(template.body)
-      super(args)
-    end
   end
 end
