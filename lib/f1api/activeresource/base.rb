11c57838571b56febbabec34ee373452ca9d51d8
Jesse Dearing <jdearing@fellowshiptech.com>
2011-06-10 17:32:10 -0500

    Monkey patched from ActiveResource
    I changed some of the classes so I had to monkey patch ActiveResource to
    respect that

--- lib/f1api/activeresource/base.rb
+++ lib/f1api/activeresource/base.rb
@@ -26,5 +26,29 @@ module FellowshipOneAPI # :nodoc:
     self.site = "#{FellowshipOneAPI::Configuration.site_url}/v1"
     # Setting mode to JSON
     self.format = :json
+
+    self.include_root_in_json = false
+
+    private
+
+    def find_or_create_resource_for(name)
+      resource_name = name.to_s.camelize
+      ancestors = self.class.name.split("::")
+      if ancestors.size > 1
+        find_resource_in_modules(resource_name, ancestors)
+      else
+        self.class.const_get(resource_name)
+      end
+    rescue NameError
+      if self.class.const_defined?(resource_name)
+        resource = self.class.const_get(resource_name)
+      else
+        resource = self.class.const_set(resource_name, Class.new(FellowshipOneAPI::Base))
+      end
+      resource.prefix = self.class.prefix
+      resource.site   = self.class.site
+      resource
+    end
+
   end
 end
