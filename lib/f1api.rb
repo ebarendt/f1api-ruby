d7c52fda742f6843d156da74ac28565480018cd8
Jesse Dearing <jesse.dearing@gmail.com>
2012-05-11 21:42:01 -0500

    Fixed loading values from yaml file

--- lib/f1api.rb
+++ lib/f1api.rb
@@ -1,16 +1,24 @@
+require 'f1api/client'
+
 module FellowshipOneAPI
   class <<self
     attr_accessor :consumer_key, :consumer_secret, :site_url, :church_code,
-      :authentication_type, :request_token_path, :access_token_path, :portal_authorize_path,
+      :request_token_path, :access_token_path, :portal_authorize_path,
       :weblink_authorize_path, :portal_credential_token_path, :weblink_credential_token_path,
       :config_file_path, :environment
 
+    attr_writer :authentication_type
+
     def configure
-      yield self
-      environment ||= :development
+      yield self if block_given?
+      self.environment ||= :development
 
       load_values_from_yml_file
       true
+    end
+
+    def authentication_type
+      @authentication_type || :oauth
     end
 
     private
@@ -18,9 +26,12 @@ module FellowshipOneAPI
       return unless config_file_path
       y = File.read(File.expand_path(config_file_path))
 
-      config = y[environment.to_s]
+      y = YAML.load(y)
+      config = y[self.environment.to_s]
       config.each do |k, v|
-        send(k.to_sym, v)
+        if send(k.to_sym).nil?
+          send("#{k}=".to_sym, v)
+        end
       end
     end
   end
